c539128b4aafb7bbaa90c2c7249bab2f52e26f8e
ShyamaHarihar <shyama.harihar@gmail.com>
2021-07-17 14:52:22 +0530

    Signup form added

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,63 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css';
-import { Link } from 'react-router-dom';
+import { Redirect } from "react-router-dom";
 import { InputGroup, InputRightElement, Input } from "@chakra-ui/input"
 import { Button, SimpleGrid, Box } from "@chakra-ui/react";
 
 const Login = () => {
-    const [show, setShow] = React.useState(false);
-    const handleClick = () => setShow(!show)
-    return (
-        <div className="form-style">
-            <SimpleGrid columns={[4, null, 1]} spacing="40px">
-                <Input
-                    variant="flushed"
-                    width="30rem"
-                    placeholder="email"
-                    focusBorderColor="white" />
-                <InputGroup size="md">
+    const [show, setShow] = useState(false);
+    const handleClick = () => setShow(!show);
+    const [redirect, setRedirect] = useState(false);
+    const redirectfun = () => {
+        if (redirect)
+            return (
+                <Redirect to='/signup'></Redirect>
+            )
+    }
+    const loginform = () => {
+        return (
+            <div className="form-style">
+                <SimpleGrid columns={[4, null, 1]} spacing="40px">
                     <Input
-                        pr="3.5rem"
                         variant="flushed"
-                        width="30rem"
-                        type={show ? "text" : "password"}
-                        placeholder="Enter password"
-                        focusBorderColor="white"
-                    />
-                    <InputRightElement width="4.5rem">
-                        <Button h="1.75rem" size="sm" color="black" onClick={handleClick}>
-                            {show ? "Hide" : "Show"}
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-                <Box
-                    as="button"
-                    height="34px"
-                    lineHeight="1.2"
-                    transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
-                    px="8px"
-                    borderRadius="5px"
-                    fontSize="14px"
-                    fontWeight="semibold"
-                    bg="#dde0e2"
-                    color="#0c0c0c"
-                    _hover={{ bg: "#ebedf0" }}
-                    _active={{
-                        bg: "#dddfe2",
-                        transform: "scale(0.98)",
-                        borderColor: "#dddddd",
-                    }}
-                    _focus={{
-                        boxShadow:
-                            "0 0 1px 2px #e3e6ebee, 0 1px 1px #47444425"
-                    }}
-                >
-                    Login
-                </Box>
-                <Button colorScheme="white" variant="outline">
-                    Don't have an Account?&nbsp;&nbsp;<Link to='/signup'>Sign up</Link>
-                </Button>
-            </SimpleGrid>
+                        width="25rem"
+                        placeholder="email"
+                        focusBorderColor="white" />
+                    <InputGroup size="md">
+                        <Input
+                            pr="3.5rem"
+                            variant="flushed"
+                            width="25rem"
+                            type={show ? "text" : "password"}
+                            placeholder="Enter password"
+                            focusBorderColor="white"
+                        />
+                        <InputRightElement width="4.5rem">
+                            <Button h="1.75rem" size="sm" color="black" onClick={handleClick}>
+                                {show ? "Hide" : "Show"}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                    <Box
+                        as="button"
+                        height="34px"
+                        lineHeight="1.2"
+                        transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+                        px="8px"
+                        borderRadius="10px"
+                        fontSize="14px"
+                        fontWeight="semibold"
+                        bg="#dde0e2"
+                        color="#0c0c0c"
+                        _hover={{ bg: "#ebedf0" }}
+                        _active={{
+                            bg: "#dddfe2",
+                            transform: "scale(0.98)",
+                            borderColor: "#dddddd",
+                        }}
+                        _focus={{
+                            boxShadow:
+                                "0 0 1px 2px #e3e6ebee, 0 1px 1px #47444425"
+                        }}
+                    >
+                        LOGIN
+            </Box>
+                    <Button
+                        colorScheme="white"
+                        variant="outline"
+                        fontWeight="light"
+                        borderRadius="10px"
+                        borderWidth="1.8px"
+                        onClick={() => { setRedirect(true); }}>
+                        DON'T HAVE AN ACCOUNT? SIGNUP
+            </Button>
+                </SimpleGrid>
+            </div>);
+
+    }
+    return (
+        <div>
+            {loginform()}
+            {redirectfun()}
         </div>
     )
 }
--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -1,88 +1,89 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
 import './style.css';
-const styleobj = { background: " rgba(3, 0, 0, 0.89)", border: "2px solid white", borderRadius: "20px", color: "white" }
+import { Redirect } from "react-router-dom";
+import { InputGroup, InputRightElement, Input, InputLeftAddon } from "@chakra-ui/input"
+import { Button, SimpleGrid, Box } from "@chakra-ui/react";
 const SignUp = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [mobile, setMobile] = useState("");
-    const [password, setPassword] = useState("");
-    return (
-        <>
+    const [show, setShow] = useState(false);
+    const handleClick = () => setShow(!show);
+    const [redirect, setRedirect] = useState(false);
+    const redirectfun = () => {
+        if (redirect)
+            return (
+                <Redirect to='/login'></Redirect>
+            )
+    }
+    const signupform = () => {
+        return (
             <div className="form-style">
-                <form className="ui form">
-                    <div className="ui two column centered grid signup-style">Sign up</div>
-                    <div className="ui two column centered grid">
-                        <div className="column">
-                            <div className='field'>
-                                <label className="labelstyle">Full Name</label>
-                                <input
-                                    type="text"
-                                    placeholder="fullname"
-                                    style={styleobj}
-                                    value={name}
-                                    onChange={e => setName(e.target.value)}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="ui two column centered grid">
-                        <div className="column">
-                            <div className="field">
-                                <label className="labelstyle">Email</label>
-                                <input
-                                    type="email"
-                                    placeholder="email"
-                                    style={styleobj}
-                                    value={email}
-                                    onChange={e => setEmail(e.target.value)}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="ui two column centered grid">
-                        <div className="column">
-                            <div className="field">
-                                <label className="labelstyle">Mobile</label>
-                                <input
-                                    type="text"
-                                    placeholder="mobile"
-                                    style={styleobj}
-                                    value={mobile}
-                                    onChange={e => setMobile(e.target.value)}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="ui two column centered grid">
-                        <div className="column">
-                            <div className="field labelstyle">
-                                <label className="labelstyle">Password</label>
-                                <input
-                                    type="password"
-                                    placeholder="password"
-                                    style={styleobj}
-                                    value={password}
-                                    onChange={e => setPassword(e.target.value)} />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="ui two column centered grid">
-                        <div className="column">
-                            <button className="btn custom-button">Create Account</button>
-                        </div>
-                    </div>
-                    <div className="ui two column centered grid">
-                        <div className="column">
-                            <label>Aldready have an account?</label>
-                            <Link to='login' style={{ color: "#FFF", fontWeight: "bolder" }}> &nbsp;&nbsp;Log in</Link>
-                        </div>
-                    </div>
-                </form>
-            </div>
+                <SimpleGrid columns={[5, null, 1]} spacing="40px">
+                    <Input
+                        variant="flushed"
+                        width="25rem"
+                        placeholder="fullname"
+                        focusBorderColor="white" />
+                    <Input
+                        variant="flushed"
+                        width="25rem"
+                        placeholder="email"
+                        focusBorderColor="white" />
+                    <InputGroup>
+                        <InputLeftAddon color="black" children="+91" />
+                        <Input type="tel" placeholder="phone number" focusBorderColor="white" />
+                    </InputGroup>
+                    <InputGroup size="md">
+                        <Input
+                            pr="3.5rem"
+                            variant="flushed"
+                            width="25rem"
+                            type={show ? "text" : "password"}
+                            placeholder="Enter password"
+                            focusBorderColor="white"
+                        />
+                        <InputRightElement width="4.5rem">
+                            <Button h="1.75rem" size="sm" color="black" onClick={handleClick}>
+                                {show ? "Hide" : "Show"}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                    <Box
+                        as="button"
+                        height="34px"
+                        lineHeight="1.2"
+                        transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+                        px="8px"
+                        borderRadius="10px"
+                        fontSize="14px"
+                        fontWeight="semibold"
+                        bg="#dde0e2"
+                        color="#0c0c0c"
+                        _hover={{ bg: "#ebedf0" }}
+                        _active={{
+                            bg: "#dddfe2",
+                            transform: "scale(0.98)",
+                            borderColor: "#dddddd",
+                        }}
+                        _focus={{
+                            boxShadow:
+                                "0 0 1px 2px #e3e6ebee, 0 1px 1px #47444425"
+                        }}
+                    >
+                        SIGN UP
+            </Box>
+                    <Button colorScheme="white" variant="outline" fontWeight="light" borderRadius="10px" borderWidth="1.8px" onClick={() => { setRedirect(true); }}>
+                        ALDREADY A USER? SIGN IN
+                    </Button>
+                </SimpleGrid>
+            </div>);
 
-        </>
+    }
+    return (
+        <div>
+            {signupform()}
+            {redirectfun()}
+        </div>
     )
 }
 
-export default SignUp;
+export default SignUp
+
